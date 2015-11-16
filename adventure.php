20a751920264f0124f24f9b078cd081b9e14cca7
Jo Beattie <beattiejo1@gmail.com>
2015-11-16 16:21:48 +0000

    updated link to stylesheet

--- adventure.php
+++ adventure.php
@@ -4,15 +4,18 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <style src="Stylesheets/adventureStyle.css"
+        <link rel="stylesheet" href="Stylesheets/adventureStyle.css" type="text/css">
     </head>
 
     <body>
+
         <div class="container-fluid" name="Title">
             <h2>Adventure by Author</h2>
         </div>
+
         <div class="container-fluid" name="content">
             <h4>This is my Adventure</h4>
         </div>
+
     </body>
 </html>
