a8ce887c695d137a4c6a19fca887012fda6cc488
Jo Beattie <beattiejo1@gmail.com>
2015-11-16 17:14:29 +0000

    added bowie's style and renamed stylesheet

--- adventure.php
+++ adventure.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="Stylesheets/adventureStyle.css" type="text/css">
+        <link rel="stylesheet" href="Stylesheets/style.css" type="text/css">
     </head>
 
     <body>
