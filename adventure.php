3973dc4a90a2ef6c7353198a46b214b04ce22acc
Jo Beattie <beattiejo1@gmail.com>
2015-11-16 18:04:31 +0000

    changed columns on content

--- adventure.php
+++ adventure.php
@@ -28,11 +28,11 @@
 
         <div class="container-fluid" id="content">
             <div class="row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-8">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-10">
                     <h4>I went on an adventure</h4>
                 </div>
-                <div class="col-sm-2"></div>
+                <div class="col-sm-1"></div>
             </div>
         </div>
 
