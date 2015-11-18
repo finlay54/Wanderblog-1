f66b7d3119cef071b888f1cf40c7ca133cf326aa
finlay54 <finlayjohnston60@gmail.com>
2015-11-18 15:14:16 +0000

    adding site navbar

--- authorsearch.php
+++ authorsearch.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="shortcut icon" href="images/earth.ico">
     <style>
         .navbar {
             margin-bottom: 0;
@@ -36,7 +38,7 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-inverse">
+<?php /*<nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
             <span class="icon-bar"></span>
@@ -55,6 +57,42 @@
         <ul class = "nav navbar-nav navbar-right">
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
         </ul>
+    </div>
+</nav>*/ ?>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="welcome.php"><span class="glyphicon glyphicon-globe"></span></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#carousel">TOP 5 TRIPS</a></li>
+                <li><a href="#" data-toggle="modal" data-target="#modal-reg">REGISTER</a></li>
+                <li>
+                    <form role="form" style="padding-top: 10px" class="form-inline" action="welcome.php" method="post">
+                        <input class="form-control" type="text" name="usernameInput" placeholder="Username">
+                        <input class="form-control" type="password" name="passwordInput" placeholder="Password">
+                        <button type="submit" class="btn btn-success">LOGIN</button>
+                    </form>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">MORE
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="about.php">ABOUT</a></li>
+                        <li><a href="adventuresearch.html">ADVENTURE SEARCH</a></li>
+                        <li><a href="authorsearch.html">AUTHOR SEARCH</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 
